b391313b14cd46ce0ffa8b732a6af7448a18f73a
Marwen Ben Taleb <m.bentaleb@quermes.net>
2026-08-16 14:02:04 +0100

    refactor: use translation key for skill start message

--- src/app/skills/skill-start/skill-start.component.ts
+++ src/app/skills/skill-start/skill-start.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skill-start',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './skill-start.component.html',
   styleUrls: ['./skill-start.component.scss'],
 })
 export class SkillStartComponent implements OnInit {
+  selectSkillText = 'ADMIN_SKILLS_START_COMPONENT.SELECT_SKILL_TEXT';
+
   constructor() {}
 
   ngOnInit() {}
